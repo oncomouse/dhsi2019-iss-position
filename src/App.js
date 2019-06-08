fe4f812f05e5d73906a1d7beff30de14b68274b6
oncomouse <oncomouse@gmail.com>
2019-06-08 09:09:16 -0700

    static rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import ISSPeople from './components/ISSPeople';
 import ISSPosition from './components/ISSPosition';
 
 const App = () => {
   return (
     <div className="wrapper-large">
+      <Helmet>
+        <title>ISS Position</title>
+      </Helmet>
       <div className="ta-center">
         <h1>ISS</h1>
         <p className="fs-small">
           This project uses <a href="https://reactjs.org/">React</a> and <a href="https://leafletjs.com/">Leaflet</a> to visualize the position of the International Space Station (ISS).
           <br />Position and Astronaut data provided by <a href="http://open-notify.org/">Open Notify</a>.
+          <br />Source code available at: <a href="https://github.com/oncomouse/dhsi2019-iss-position">https://github.com/oncomouse/dhsi2019-iss-position</a>.
         </p>
       </div>
       <h2 className="ta-center">Where is the ISS?</h2>
